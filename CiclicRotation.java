5650981f9b6c3ec0110a8c0374619655ab419530
Igor Medeiros <igor.medeiros@gmail.com>
2022-07-11 23:49:18 -0300

    CiclicRotation.java

---
+++ CiclicRotation.java
@@ -0,0 +1,21 @@
+public class CiclicRotation{
+    public static void main(String[] args) {
+        
+    }
+}
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int[] solution(int[] A, int K) {
+        // write your code in Java SE 8
+        int[] B = new int[A.length];
+        for (int i = 0; i < A.length; i++) {
+            B[(i + K) % A.length] = A[i];
+        }
+        return B;
+    }
+}
